8a0692294347ee42a0266c04a9eeda36b212ecc7
tbekos <tbekos@gmail.com>
2013-03-26 17:24:38 +0200

    Propose refactor for dropdown toggle.
     * Restict on EA (dropdown-toggle)
     * Use template to render bootstrap's html
      (`open` is not hardcoded in the code).
     * Add option for dropdown to be initially open.
     * Add tests & demo examples.

--- src/dropdownToggle/dropdownToggle.js
+++ src/dropdownToggle/dropdownToggle.js
@@ -1,60 +1,141 @@
 /*
  * dropdownToggle - Provides dropdown menu functionality in place of bootstrap js
- * @restrict class or attribute
- * @example:
-   <li class="dropdown">
-     <a class="dropdown-toggle">My Dropdown Menu</a>
-     <ul class="dropdown-menu">
-       <li ng-repeat="choice in dropChoices">
-         <a ng-href="{{choice.href}}">{{choice.text}}</a>
-       </li>
-     </ul>
-   </li>
  */
 
-angular.module('ui.bootstrap.dropdownToggle', []).directive('dropdownToggle', 
-['$document', '$location', '$window', function ($document, $location, $window) {
-  var openElement = null, close;
+angular.module('ui.bootstrap.dropdownToggle', [])
+
+.factory('dropdownOpenScope', function() {
+  var _scope = null;
+
   return {
-    restrict: 'CA',
-    link: function(scope, element, attrs) {
-      scope.$watch(function dropdownTogglePathWatch(){return $location.path();}, function dropdownTogglePathWatchAction() {
-        if (close) { close(); }
-      });
-
-      element.parent().bind('click', function(event) {
-        if (close) { close(); }
-      });
-
-      element.bind('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        var iWasOpen = false;
-
-        if (openElement) {
-          iWasOpen = openElement === element;
-          close();
-        }
-
-        if (!iWasOpen){
-          element.parent().addClass('open');
-          openElement = element;
-
-          close = function (event) {
-            if (event) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            $document.unbind('click', close);
-            element.parent().removeClass('open');
-            close = null;
-            openElement = null;
-          };
-
-          $document.bind('click', close);
-        }
-      });
+    get: function() {
+      return _scope;
+    },
+    set: function(scope) {
+      _scope = scope;
+    },
+    clear: function() {
+      _scope = null;
     }
   };
-}]);
+})
+
+.controller('DropdownToggleController', ['$scope', '$element', '$attrs', '$document', '$location', 'dropdownOpenScope',
+  function (scope, element, attrs, $document, $location, dropdownOpenScope) {
+    var initiallyOpen = (angular.isDefined(attrs.opened)) ? scope.$eval(attrs.opened) : false;
+
+    scope.isOpen = function() {
+      return ( scope.open === true );
+    };
+
+    scope.toggleClose = function(isDocumentClick) {
+      scope.open = false;
+      $document.unbind('click', scope.toggleClose);
+      dropdownOpenScope.clear();
+
+      if ( isDocumentClick !== false ) {
+        scope.$apply();
+      }
+    };
+
+    scope.toggleOpen = function() {
+      scope.open = true;
+      $document.bind('click', scope.toggleClose);
+
+      var openScope = dropdownOpenScope.get();
+
+      if ( openScope ) {
+        openScope.toggleClose(false);
+      }
+      dropdownOpenScope.set(scope);
+    };
+
+    scope.toggle = function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      // Check if i was open
+      if ( scope.isOpen() ) {
+        scope.toggleClose(false);
+      } else {
+        scope.toggleOpen();
+      }
+      scope.onToggle({ open: scope.isOpen() });
+    };
+
+    scope.$watch(function dropdownTogglePathWatch() {
+      return $location.path();
+    }, function dropdownTogglePathWatchAction(newPath, oldPath) {
+      if ( newPath !== oldPath && scope.isOpen() ) {
+        scope.toggleClose(false);
+      }
+    });
+
+    if ( initiallyOpen ) {
+        scope.toggleOpen();
+    }
+}])
+
+.directive('dropdownToggle', function () {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    scope: {
+      heading: '@',
+      placement: '@',
+      onToggle: '&'
+    },
+    controller: 'DropdownToggleController',
+    templateUrl: 'template/dropdownToggle/dropdownToggle.html',
+    replace: true
+  };
+})
+
+.directive('dropdownList', function () {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    scope: {
+      heading: '@',
+      placement: '@',
+      onToggle: '&'
+    },
+    controller: 'DropdownToggleController',
+    templateUrl: 'template/dropdownToggle/dropdownList.html',
+    replace: true
+  };
+})
+
+.directive('dropdownButton', function () {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    scope: {
+      heading: '@',
+      placement: '@',
+      type: '@',
+      size: '@',
+      onToggle: '&'
+    },
+    controller: 'DropdownToggleController',
+    templateUrl: 'template/dropdownToggle/dropdownButton.html',
+    replace: true
+  };
+})
+
+.directive('dropdownSplitButton', function () {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    scope: {
+      heading: '@',
+      placement: '@',
+      type: '@',
+      size: '@',
+      onToggle: '&'
+    },
+    controller: 'DropdownToggleController',
+    templateUrl: 'template/dropdownToggle/dropdownSplitButton.html',
+    replace: true
+  };
+});
--- src/dropdownToggle/test/dropdownToggleSpec.js
+++ src/dropdownToggle/test/dropdownToggleSpec.js
@@ -1,67 +1,153 @@
 describe('dropdownToggle', function() {
-  var $compile, $rootScope, $document, $location;
+  var $compile, $rootScope, $document, $location, element;
 
   beforeEach(module('ui.bootstrap.dropdownToggle'));
+  beforeEach(module('template/dropdownToggle/dropdownToggle.html'));
 
   beforeEach(inject(function(_$compile_, _$rootScope_, _$document_, _$location_) {
     $compile = _$compile_;
     $rootScope = _$rootScope_;
     $document = _$document_;
     $location = _$location_;
-
+    element = $compile('<dropdown-toggle heading="Click me"><li>Hello 1</li><li>Hello 2</li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
   }));
 
-  function dropdown() {
-    return $compile('<li class="dropdown"><a dropdown-toggle></a><ul dropdown-toggle><li>Hello</li></ul></li>')($rootScope);
+  function toggleElement(_element) {
+    return element.find('a');
   }
+
+  it('should contain the appropriate elements', function() {
+     expect(element.hasClass('dropdown')).toBe(true);
+
+     expect(element.find('a').length).toBe(1);
+     expect(element.find('a').hasClass('dropdown-toggle')).toBe(true);
+     expect(element.find('ul').length).toBe(1);
+     expect(element.find('ul').hasClass('dropdown-menu')).toBe(true);
+     expect(element.find('li').length).toBe(2);
+  });
   
   it('should toggle on `a` click', function() {
-    var elm = dropdown();
-    expect(elm.hasClass('open')).toBe(false);
-    elm.find('a').click();
-    expect(elm.hasClass('open')).toBe(true);
-    elm.find('a').click();
-    expect(elm.hasClass('open')).toBe(false);
+    var toggleEl = toggleElement();
+
+    expect(element.hasClass('open')).toBe(false);
+    toggleEl.click();
+    expect(element.hasClass('open')).toBe(true);
+    toggleEl.click();
+    expect(element.hasClass('open')).toBe(false);
   });
 
-  it('should toggle on `ul` click', function() {
-    var elm = dropdown();
-    expect(elm.hasClass('open')).toBe(false);
-    elm.find('ul').click();
-    expect(elm.hasClass('open')).toBe(true);
-    elm.find('ul').click();
-    expect(elm.hasClass('open')).toBe(false);
-  });
-
-  it('should close on elm click', function() {
-    var elm = dropdown();
-    elm.find('a').click();
-    elm.click();
-    expect(elm.hasClass('open')).toBe(false);
-  });
 
   it('should close on document click', function() {
-    var elm = dropdown();
-    elm.find('a').click();
+    var toggleEl = toggleElement();
+
+    expect(element.hasClass('open')).toBe(false);
+    toggleEl.click();
+    expect(element.hasClass('open')).toBe(true);
     $document.click();
-    expect(elm.hasClass('open')).toBe(false);
+    expect(element.hasClass('open')).toBe(false);
   });
 
   it('should close on $location change', function() {
-    var elm = dropdown();
-    elm.find('a').click();
+    element.find('a').click();
     $location.path('/foo');
     $rootScope.$apply();
-    expect(elm.hasClass('open')).toBe(false);
+    expect(element.hasClass('open')).toBe(false);
   });
 
   it('should only allow one dropdown to be open at once', function() {
-    var elm1 = dropdown();
-    var elm2 = dropdown();
-    elm1.find('a').click();
-    elm2.find('a').click();
-    expect(elm1.hasClass('open')).toBe(false);
-    expect(elm2.hasClass('open')).toBe(true);
+    var element2 = $compile('<dropdown-toggle heading="Click me 2"><li>New 1</li><li>New 2</li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
+    var toggleEl = toggleElement();
+    var toggleEl2 = element2.find('a');
+
+    expect(element.hasClass('open')).toBe(false);
+    expect(element2.hasClass('open')).toBe(false);
+    toggleEl.click();
+
+    expect(element.hasClass('open')).toBe(true);
+    expect(element2.hasClass('open')).toBe(false);
+    toggleEl2.click();
+
+    expect(element.hasClass('open')).toBe(false);
+    expect(element2.hasClass('open')).toBe(true);
   });
+
+  it('should initially open element from attributes state', function() {
+    element = $compile('<dropdown-toggle heading="Click me" opened="true"><li></li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
+
+    expect(element.hasClass('open')).toBe(true);
+  });
+
+  it('should only allow one element to be initially open', function() {
+    element = $compile('<dropdown-toggle heading="Click me" opened="true"><li></li></dropdown-toggle>')($rootScope);
+    var element2 = $compile('<dropdown-toggle heading="Click me 2" opened="true"><li></li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
+
+    expect(element.hasClass('open')).toBe(false);
+    expect(element2.hasClass('open')).toBe(true);
+  });
+
+  it('executes the inner function on click and closes the dropdown', function() {
+    $rootScope.listHandler = jasmine.createSpy('listHandler');
+    element = $compile('<dropdown-toggle heading="" opened="true"><li ng-click="listHandler()" class="item"></li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
+
+    expect(element.hasClass('open')).toBe(true);
+    expect($rootScope.listHandler).not.toHaveBeenCalled();
+
+    var li = element.find('li');
+    li.click();
+    $document.click();
+    $rootScope.$digest();
+
+    expect(element.hasClass('open')).toBe(false);
+    expect($rootScope.listHandler).toHaveBeenCalled();
+  });
+
+  it('executes the onToggle expression every time the menu opens or closes', function() {
+    $rootScope.toggleHandler = jasmine.createSpy('toggleHandler');
+    element = $compile('<dropdown-toggle heading="" on-toggle="toggleHandler(open)"><li></li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
+    var toggleEl = toggleElement();
+
+    expect(element.hasClass('open')).toBe(false);
+    expect($rootScope.toggleHandler).not.toHaveBeenCalled();
+
+    toggleEl.click();
+    expect(element.hasClass('open')).toBe(true);
+    expect($rootScope.toggleHandler).toHaveBeenCalledWith(true);
+
+    toggleEl.click();
+    expect(element.hasClass('open')).toBe(false);
+    expect($rootScope.toggleHandler).toHaveBeenCalledWith(false);
+  });
+
+  it('executes other document click events normally', function() {
+    var checkboxEl = $compile('<input type="checkbox" ng-click="clicked = true" />')($rootScope);
+    $rootScope.$digest();
+    var toggleEl = toggleElement();
+
+    expect(element.hasClass('open')).toBe(false);
+    expect($rootScope.clicked).toBeFalsy();
+
+    toggleEl.click();
+    $rootScope.$digest();
+    expect(element.hasClass('open')).toBe(true);
+    expect($rootScope.clicked).toBeFalsy();
+
+    checkboxEl.click();
+    $rootScope.$digest();
+    expect($rootScope.clicked).toBeTruthy();
+  });
+
+  it('handles correctly placement attribute', function() {
+    element = $compile('<dropdown-toggle heading="" placement="right"><li></li></dropdown-toggle>')($rootScope);
+    $rootScope.$digest();
+
+    expect(element.find('ul').hasClass('pull-right')).toBe(true);
+  });
+
 });
   
